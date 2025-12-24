f44e02f607dd32102454f6fba9faac9f56f4d0f1
qgmbek <100270972+qgmbek@users.noreply.github.com>
2025-12-24 16:16:02 +0500

    clean up

--- README.md
+++
@@ -1,8 +0,0 @@
-Working Version of Page Transition
-
-Uploaded 2024.01.15
-
-Next.js
-JavaScript
-Framer Motion
-
--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,18 @@
-'use client';
+"use client";
 
-import React from "react";
 // import { AnimatePresence } from "framer-motion";
-
-// Pages
 import Land from "./land";
-
-// Components
-import Header from "./header";
-
-//Styles
 import "./App.scss";
-import "./globals.css";
 
 export default function Home() {
-    const imageDetails = {
-      width: 524,
-      height: 550,
-    };
-  
-    return (
-      <>
-          <Header />
-          <Land imageDetails={imageDetails} />
-      </>
-    );
+  const imageDetails = {
+    width: 524,
+    height: 550,
+  };
+
+  return (
+    <>
+      <Land imageDetails={imageDetails} />
+    </>
+  );
 }
